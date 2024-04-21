a81c4787c135ea5791f77cc4535d406a1f206223
unknown <dassudeshna96@gmail.com>
2024-04-22 02:16:40 +0530

    added javascript and API to fetch data

---
+++ index.js
@@ -0,0 +1,32 @@
+var inputvalue = document.querySelector("#cityinput")
+var btn = document.querySelector("#add");
+var city = document.querySelector("#cityoutput")
+var descrip = document.querySelector("#description")
+var temp = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+apik = "34f0da6851d1a4791a3112c4e64f99cb"
+function convertion(val) {
+    return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click', function()
+{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+    .then(res => res.json())
+
+    .then(data =>
+     {
+        var nameval = data['name']
+        var descrip = data['weather']['0']['description']
+        var temperature = data['main']['temp']
+        var windspeed = data['speed']
+       
+
+        city.innerHTML = 'Weather of <span> ${nameval} </span>'
+        temp.innerHTML = 'Temperature: <span> ${ convertion(temperature)} C </span>'
+        description.innerHTML = 'Sky Conditions: <span> ${desc} </span>'
+        wind.innerHTML = 'Wind Speed: <span>${windspeed} km/hr </span>'
+    })
+
+    .catch(err => alert('You entered wrond city name'))
+})
