be094a5c965c5c056f4e413e57bafe9fd8440bec
unknown <dassudeshna96@gmail.com>
2024-04-22 17:02:46 +0530

    changed styling

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ function convertion(val) {
 
 btn.addEventListener('click', function()
 {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+ apik)
     .then(res => res.json())
 
     .then(data =>
      {
         var nameval = data['name']
-        var descrip = data['weather']['0']['description']
+        var desc = data['weather']['0']['description']
         var temperature = data['main']['temp']
-        var windspeed = data['speed']
+        var windspeed = data['wind']['speed']
        
 
-        city.innerHTML = 'Weather of <span> ${nameval} </span>'
-        temp.innerHTML = 'Temperature: <span> ${ convertion(temperature)} C </span>'
-        description.innerHTML = 'Sky Conditions: <span> ${desc} </span>'
-        wind.innerHTML = 'Wind Speed: <span>${windspeed} km/hr </span>'
+        city.innerHTML = `Weather of <span>${nameval} </span>`
+        temp.innerHTML = `Temperature: <span> ${ convertion(temperature)} C </span>`
+        description.innerHTML = `Sky Conditions: <span> ${desc} </span>`
+        wind.innerHTML = `Wind Speed: <span>${windspeed} km/hr </span>`
     })
 
-    .catch(err => alert('You entered wrond city name'))
+    .catch(err => alert('You entered wrong city name'))
 })
